ee1989afb5828f1ddb95bb264bd006327e57a263
DaidoujiChen <daidoujichen@gmail.com>
2016-06-17 14:33:05 +0800

    update to 2.1.5

---
+++ VMFiveAdNetwork/VMFiveAdNetwork/VAAdRewardedVideo.h
@@ -0,0 +1,51 @@
+//
+//  VAAdRewardedVideo.h
+//  AdnSDK
+//
+//  Created by DaidoujiChen on 2016/5/12.
+//  Copyright © 2016年 VMFive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "VAAdView.h"
+
+@protocol VAAdRewardedVideoDelegate;
+
+@interface VAAdRewardedVideo : UIViewController <UIPopoverControllerDelegate>
+
+@property (nonnull, nonatomic, readonly) NSString *placement;
+@property (nonnull, nonatomic, copy) NSString *apiKey;
+@property (nullable, nonatomic, weak) id<VAAdRewardedVideoDelegate> delegate;
+@property (nonatomic, assign) VAAdType adType;
+@property (nonatomic, assign) BOOL testMode;
+
+-(nonnull instancetype)initWithplacement:(nonnull NSString *)placement;
+-(void)loadAd;
+-(void)show;
+
+@end
+
+@interface VAAdRewarded : NSObject
+
+@property (nonnull, nonatomic, copy) NSString *currencyType;
+@property (nonnull, nonatomic) NSNumber *amount;
+
+@end
+
+@protocol VAAdRewardedVideoDelegate <NSObject>
+
+@optional
+-(void)rewardedVideoDidLoad:(nonnull VAAdRewardedVideo *)rewardedVideo;
+-(void)rewardedVideoWillShow:(nonnull VAAdRewardedVideo *)rewardedVideo;
+-(void)rewardedVideoDidShow:(nonnull VAAdRewardedVideo *)rewardedVideo;
+
+-(void)rewardedVideoWillClose:(nonnull VAAdRewardedVideo *)rewardedVideo;
+-(void)rewardedVideoDidClose:(nonnull VAAdRewardedVideo *)rewardedVideo;
+
+-(void)rewardedVideoDidClick:(nonnull VAAdRewardedVideo *)rewardedVideo;
+-(void)rewardedVideoDidFinishHandlingClick:(nonnull VAAdRewardedVideo *)rewardedVideo;
+-(void)rewardedVideo:(nonnull VAAdRewardedVideo *)rewardedVideo shouldReward:(nonnull VAAdRewarded *)rewarded;
+
+-(void)rewardedVideo:(nonnull VAAdRewardedVideo *)rewardedVideo didFailWithError:(nonnull NSError *)error;
+
+@end
